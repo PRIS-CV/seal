6d30f6c2c546da236d251b099871e30214d849a4
xin-ran-w <wangxr@bupt.edu.cn>
2023-08-18 13:33:02 +0800

    delete old eval system

--- seal/utils/utils.py
+++ seal/utils/utils.py
@@ -62,3 +62,20 @@ def convert_cfg_to_dict(root_node):
 
     self_as_dict = convert_to_dict(root_node, [])
     return self_as_dict
+
+
+def get_constr_out(x, R):
+    r"""
+        Given the output of the neural network x, this function 
+        returns the output of MCM given the hierarchy constraint 
+        expressed in the adjacency matrix R. 
+    """
+
+    
+    
+    c_out = x
+    c_out = c_out.unsqueeze(1)
+    c_out = c_out.expand(len(x), R.shape[1], R.shape[1])
+    R_batch = R.expand(len(x), R.shape[1], R.shape[1])
+    final_out, max_index = torch.max(R_batch * c_out.double(), dim = 2)
+    return final_out, max_index
